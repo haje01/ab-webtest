67618b0819df7e1d0fced26f10b186e2ab7bc455
JeongJu Kim <haje01@gmail.com>
2023-04-28 14:35:59 +0900

    유저 정보 생성/읽기

--- client/client.py
+++ client/client.py
@@ -1,3 +1,4 @@
+import os
 import asyncio
 import random
 
@@ -9,12 +10,26 @@ USER_CNT = 50
 fake = Faker()
 
 users = []
-for i in range(USER_CNT):
-    game_id = fake.unique.random_int(min=111111, max=999999)
-    plat_id = fake.unique.random_int(min=111111, max=999999)
-    email = fake.email()
-    users.append([game_id, plat_id, email])
-
+# /data 에 유저 정보 파일이 없으면 생성
+if not os.path.isfile('/data/users.csv'):
+    print("create /data/users.csv")
+    with open('/data/users.csv', 'w') as f:
+        # 유저 정보 생성
+        for i in range(USER_CNT):
+            game_id = fake.unique.random_int(min=111111, max=999999)
+            plat_id = fake.unique.random_int(min=111111, max=999999)
+            email = fake.email()
+            user = (game_id, plat_id, email)
+            users.append(user)
+            # 유저 정보 저장
+            f.write(','.join(map(str, user)) + '\n')
+# 있으면 읽어서 유저 정보 리스트 생성
+else:
+    print("load /data/users.csv")
+    with open('/data/users.csv', 'r') as f:
+        for line in f:
+            user = line.strip().split(',')
+            users.append(user)
 
 def handle_console_msg(msg):
     print(f"{msg.type}: {msg.text}")
